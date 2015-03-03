b089f55b23fe702603e90a5188999f3b4cd3e2b6
renankuba <renankuba@kuba.uco.local>
2015-03-03 01:39:09 -0600

    add tags on upload

--- DJ-DRMR/src/edu/uco/sdd/spring15/dj_drmr/UploadActivity.java
+++ DJ-DRMR/src/edu/uco/sdd/spring15/dj_drmr/UploadActivity.java
@@ -1,7 +1,6 @@
 package edu.uco.sdd.spring15.dj_drmr;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
@@ -9,16 +8,24 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 public class UploadActivity extends Activity {
 	private Button btUpload;
 	private TextView txtFileChose;
+	private EditText editTextTags;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,7 @@ public class UploadActivity extends Activity {
 		
 		this.btUpload = (Button) findViewById(R.id.btnUpload);
 		this.txtFileChose = (TextView) findViewById(R.id.txtFileChose);
+		this.editTextTags = (EditText) findViewById(R.id.editTextTags);
 		
 		this.btUpload.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -41,6 +49,34 @@ public class UploadActivity extends Activity {
 			        }
 			}
 		});
+		
+		
+		this.editTextTags.setOnFocusChangeListener(new OnFocusChangeListener() {          
+		    public void onFocusChange(View v, boolean hasFocus) {
+		        if(!hasFocus) {
+		        	String text = editTextTags.getText().toString();
+		        	text = text.replaceAll("[^a-zA-Z ]+", "");
+		        	text = text.replaceAll("[ ]{2,}", " ");
+		        	String[] allTags = text.split(" ");
+		        	text = "";
+		        	for (String tag : allTags) {
+						tag = "#"+tag;
+						text = text + tag + " ";
+					}
+		        	editTextTags.setText(text);
+		        }
+		    }
+		});
+		
+		this.editTextTags.setOnEditorActionListener(new OnEditorActionListener() {
+		    @Override
+		    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		        if (actionId == EditorInfo.IME_ACTION_DONE) {
+		           editTextTags.clearFocus();
+		        }
+		        return false;
+		    }
+		});
 	}
 	
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -49,6 +85,15 @@ public class UploadActivity extends Activity {
 	        Uri uri = data.getData();
 	        String path = uri.getPath();
 	        File input = new File(path);
+	        
+//	        File sdCardRoot = Environment.getExternalStorageDirectory();
+//	        File yourDir = new File(sdCardRoot, path);
+//	        for (File f : yourDir.listFiles()) {
+//	            if (f.isFile()){
+//	            	System.out.println(f.getName());
+//	            }
+//	        }
+	        
 	        txtFileChose.setText(input.getName());
 	    }           
 	    super.onActivityResult(requestCode, resultCode, data);
